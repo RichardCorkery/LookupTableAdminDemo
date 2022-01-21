ae787182015472b377289eccb21687cd1a7ec35f
Richard Corkery <RichardCorkery@hotmail.com>
2022-01-20 21:25:03 -0500

    Basic Get, Get 1 and Insert processing

--- LookupTableAdminDemo.Api/Controllers/LookupNameValuePairsController.cs
+++ LookupTableAdminDemo.Api/Controllers/LookupNameValuePairsController.cs
@@ -3,9 +3,9 @@ using Microsoft.Azure.Cosmos.Table;
 
 namespace LookupTableAdminDemo.Api.Controllers
 {
-    
+    //ToDo: Constants
+    [Route("api/[controller]")]
     [ApiController]
-    [Route("[controller]")]
     public class LookupNameValuePairsController : ControllerBase
     {
         private readonly ILogger<LookupNameValuePairsController> _logger;
@@ -15,13 +15,24 @@ namespace LookupTableAdminDemo.Api.Controllers
             _logger = logger;
         }
 
+        // ToDo: Add api
+        // GET: api/vpb-delegates
+
         //ToDo: Get cnnstring from config file
         //ToDo: Deploy to Azure
+        //ToDo: Delete 
+        //ToDo: Create
+        //ToDo: Update/Replace
+
+        //ToDo: Confirm Delete
+        //ToDo: Split up Controller to: Controller and Repo
+
+        // GET: api/lookupnamevaluepairs
         [HttpGet()]
         public IEnumerable<LookupNameValuePairEntity> Get()
         {
             var storageAccount = CloudStorageAccount.Parse("");
-
+            
             var tableClient = storageAccount.CreateCloudTableClient();
 
             var _table = tableClient.GetTableReference("LookupNameValuePair");
@@ -37,6 +48,41 @@ namespace LookupTableAdminDemo.Api.Controllers
 
             return entities.ToArray();
         }
+
+        // GET: api/lookupnamevaluepairs/00000
+        [HttpGet("partitionKey, rowKey")]
+        public void Get(string partitionKey, string rowKey)
+        {
+            var storageAccount = CloudStorageAccount.Parse("");
+
+            var tableClient = storageAccount.CreateCloudTableClient();
+
+            var _table = tableClient.GetTableReference("LookupNameValuePair");
+
+            var operation = TableOperation.Retrieve<LookupNameValuePairEntity>(partitionKey, rowKey);
+
+            var result = _table.Execute(operation);
+
+            //return result.Result as TodoEntity;
+        }
+
+        // POST api/lookupnamevaluepairs
+        [HttpPost]
+        public void Post(LookupNameValuePairEntity model)
+        {
+            var storageAccount = CloudStorageAccount.Parse("");
+
+            var tableClient = storageAccount.CreateCloudTableClient();
+
+            var _table = tableClient.GetTableReference("LookupNameValuePair");
+
+            var entity = model;
+
+            var operation = TableOperation.InsertOrReplace(entity);
+
+            _table.Execute(operation);
+        }
+
     }
 
     public class LookupNameValuePairEntity : TableEntity
