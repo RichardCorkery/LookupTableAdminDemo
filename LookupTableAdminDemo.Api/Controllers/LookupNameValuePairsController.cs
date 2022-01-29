5eb272e360012ef12983937ff9427d5fcdb742a9
Richard Corkery <RichardCorkery@hotmail.com>
2022-01-29 07:12:26 -0500

    Repo Get All Set up

--- LookupTableAdminDemo.Api/Controllers/LookupNameValuePairsController.cs
+++ LookupTableAdminDemo.Api/Controllers/LookupNameValuePairsController.cs
@@ -1,3 +1,6 @@
+using LookupTableAdminDemo.Api.Entities;
+using LookupTableAdminDemo.Api.Models;
+using LookupTableAdminDemo.Api.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Cosmos.Table;
 
@@ -23,101 +26,99 @@ namespace LookupTableAdminDemo.Api.Controllers
 
             _table = tableClient.GetTableReference("LookupNameValuePair");
         }
-        
+
         // GET: api/vpb-delegates
+
+        //ToDo: Create repo
+        //ToDo: Split up Controller to: Controller and Repo
 
         //ToDo: Deploy to Azure
 
         //ToDo: Review PS ToDo App
         //ToDo: Confirm Delete
-        //ToDo: Split up Controller to: Controller and Repo
         //ToDo: Set up DI
         //ToDo: Review Controller of the function app I did
 
         // GET: api/lookupnamevaluepairs
-        [HttpGet()]
-        public IEnumerable<LookupNameValuePairEntity> Get()
+        [HttpGet]
+        public IEnumerable<LookupNameValuePairModel> Get()
         {
-            var query = new TableQuery<LookupNameValuePairEntity>();
+            var cnnStr = _configuration["Data:AzureStorageDemos:ConnectionString"];
 
-            var entities = _table.ExecuteQuery(query);
+            var repository = new LookupNameValuePairRepository(cnnStr);
 
-            return entities.ToArray();
+            var entities = repository.All();
+
+            var models = entities.Select(x => new LookupNameValuePairModel
+            {
+                RowKey = x.RowKey,
+                PartitionKey = x.PartitionKey,
+                LookupKey = x.LookupKey,
+                Value = x.Value
+            });
+
+            return models.ToArray();
         }
 
-        // GET: api/lookupnamevaluepairs/00000
-        [HttpGet("partitionKey, rowKey")]
-        public LookupNameValuePairEntity Get(string partitionKey, string rowKey)
-        {
+        //// GET: api/lookupnamevaluepairs/00000
+        //[HttpGet("partitionKey, rowKey")]
+        //public LookupNameValuePairModel Get(string partitionKey, string rowKey)
+        //{
 
-            var operation = TableOperation.Retrieve<LookupNameValuePairEntity>(partitionKey, rowKey);
+        //    var operation = TableOperation.Retrieve<LookupNameValuePairModel>(partitionKey, rowKey);
 
-            var result = _table.Execute(operation);
+        //    var result = _table.Execute(operation);
 
-            return result.Result as LookupNameValuePairEntity;
-        }
+        //    return result.Result as LookupNameValuePairModel;
+        //}
 
-        // POST api/lookupnamevaluepairs
-        [HttpPost]
-        public void Post(LookupNameValuePairEntity model)
-        {
-            var entity = model;
+        //// POST api/lookupnamevaluepairs
+        //[HttpPost]
+        //public void Post(LookupNameValuePairModel model)
+        //{
+        //    var entity = model;
             
 
-            var operation = TableOperation.Insert(entity);
+        //    var operation = TableOperation.Insert(entity);
 
-            //$$$RAC: Note post and put could be combined
-            //var operation = TableOperation.InsertOrReplace(entity);
+        //    //$$$RAC: Note post and put could be combined
+        //    //var operation = TableOperation.InsertOrReplace(entity);
 
-            _table.Execute(operation);
-        }
+        //    _table.Execute(operation);
+        //}
 
-        // PUT api/lookupnamevaluepairs/???
-        [HttpPut]
-        public void Put(LookupNameValuePairEntity model)
-        {
+        //// PUT api/lookupnamevaluepairs/???
+        //[HttpPut]
+        //public void Put(LookupNameValuePairModel model)
+        //{
 
-            var operation = TableOperation.Retrieve<LookupNameValuePairEntity>(model.PartitionKey, model.RowKey);
+        //    var operation = TableOperation.Retrieve<LookupNameValuePairModel>(model.PartitionKey, model.RowKey);
 
-            var result = _table.Execute(operation);
+        //    var result = _table.Execute(operation);
 
-            var entity = result.Result as LookupNameValuePairEntity;
+        //    var entity = result.Result as LookupNameValuePairModel;
             
-            entity.LookupKey = model.LookupKey;
-            entity.Value = model.Value;
+        //    entity.LookupKey = model.LookupKey;
+        //    entity.Value = model.Value;
             
-            var operation2 = TableOperation.Replace(entity);
+        //    var operation2 = TableOperation.Replace(entity);
 
-            _table.Execute(operation2);
-        }
+        //    _table.Execute(operation2);
+        //}
 
-        // PUT api/lookupnamevaluepairs/???
-        [HttpDelete]
-        public void Delete(string partitionKey, string rowKey)
-        {
-            var operation = TableOperation.Retrieve<LookupNameValuePairEntity>(partitionKey, rowKey);
+        //// PUT api/lookupnamevaluepairs/???
+        //[HttpDelete]
+        //public void Delete(string partitionKey, string rowKey)
+        //{
+        //    var operation = TableOperation.Retrieve<LookupNameValuePairModel>(partitionKey, rowKey);
 
-            var result = _table.Execute(operation);
+        //    var result = _table.Execute(operation);
 
-            var entity = result.Result as LookupNameValuePairEntity;
+        //    var entity = result.Result as LookupNameValuePairModel;
 
-            var operation2 = TableOperation.Delete(entity);
+        //    var operation2 = TableOperation.Delete(entity);
 
-            _table.Execute(operation2);
-        }
-
+        //    _table.Execute(operation2);
+        //}
     }
-
-    public class LookupNameValuePairEntity : TableEntity
-    {
-        public string PartitionKey { get; set; }
-
-        public string RowKey { get; set; }
-
-        public string LookupKey { get; set; }
-
-        public string Value { get; set; }
-
-    }
-
 }
