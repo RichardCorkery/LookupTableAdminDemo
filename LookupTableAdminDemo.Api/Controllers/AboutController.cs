02b6c7f3399c4fbe5671f2fb6a13d98326ebff37
Richard Corkery <RichardCorkery@hotmail.com>
2022-02-24 08:17:08 -0500

    Created a About Controller for Integration tests

---
+++ LookupTableAdminDemo.Api/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LookupTableAdminDemo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        // GET: api/<AboutController>
+        [HttpGet]
+        public string Get()
+        {
+            return "About Controller";
+        }
+    }
+}
